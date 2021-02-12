9e95fa0b684304e5764d3e6a79f324f3e8a90da2
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-12 08:09:33 -0500

    Install and configure devise

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 ruby "3.0.0"
 
 gem "bootsnap", ">= 1.4.4", require: false
+gem "devise"
 gem "jbuilder", "~> 2.7"
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stridecatcher_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'cs-stridecatcher.herokuapp.com' }
+
+
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
