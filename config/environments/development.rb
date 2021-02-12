a7bb2ca4a8a558df351b3653a49603654387d9a1
Chuck <chuck@eclecticsaddlebag.com>
2021-02-12 07:47:27 -0500

    Install and configure bullet and strong_migrations (#1)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -73,4 +68,11 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # https://github.com/flyerhzm/bullet
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+  end
+
 end
