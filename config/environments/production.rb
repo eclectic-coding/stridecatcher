ad53d088b576facca02353448f3f78d69b2b7eb3
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-12 09:40:11 -0500

    Configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'cs-stridecatcher.herokuapp.com' }
-
-
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'cs-stridecatcher.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
