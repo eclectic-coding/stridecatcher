e6ebc2e09b64173d6fd794f4437bb56e1e1a09fa
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-13 17:25:11 -0500

    Write validations for distance and duration

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,4 +6,6 @@ class Activity < ApplicationRecord
   enum unit: { miles: 0, kilometers: 1, meters: 2, yards: 3 }
 
   validates :date, presence: true
+  validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
+  validates :distance, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
 end
