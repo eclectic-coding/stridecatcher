de5ec6e48fc5a993a9d1fe39038616c8b347a5df
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-13 15:25:39 -0500

    Add enum values to activities, and associate with user

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,7 @@
 class Activity < ApplicationRecord
+  belongs_to :user
+
+  enum category: { run: 0, long_run: 1, workout: 2, race: 3, other: 4 }
+  enum difficulty: { easy: 0, moderate: 1, hard: 2 }
+  enum unit: { miles: 0, kilometers: 1, meters: 2, yards: 3 }
 end
