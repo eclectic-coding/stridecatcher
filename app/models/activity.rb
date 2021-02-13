e312d2fda3a832ee4073aa58abbbfd0795b1910b
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-13 17:18:55 -0500

    Write simple validations for Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,6 @@ class Activity < ApplicationRecord
   enum category: { run: 0, long_run: 1, workout: 2, race: 3, other: 4 }
   enum difficulty: { easy: 0, moderate: 1, hard: 2 }
   enum unit: { miles: 0, kilometers: 1, meters: 2, yards: 3 }
+
+  validates :date, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(email: "uniquer@example.com", password: "123456", password_confirmation: "123456")
+    @user_with_activities = users(:confirmed_user_with_activities)
   end
 
   test "should be valid" do
@@ -12,6 +13,12 @@ class UserTest < ActiveSupport::TestCase
   test "should have default value of UTC in time_zone" do
     @user.save
     assert_equal "UTC", @user.reload.time_zone
+  end
+
+  test "should delete associated activities" do
+    assert_difference("Activity.count", -"#{@user_with_activities.activities.count}".to_i) do
+      @user_with_activities.destroy
+    end
   end
 
 end
