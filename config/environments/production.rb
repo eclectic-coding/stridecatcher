07509e2a36855713edb87480dbcfb7ba087413f1
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-14 10:50:35 -0500

    Update Sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,23 +63,25 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stridecatcher_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'cs-stridecatcher.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'cs-stridecatcher.herokuapp.com',
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  host = 'cs-stridecatcher.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
-    :authentication => :plain,
+    :authentication => 'apikey',
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
