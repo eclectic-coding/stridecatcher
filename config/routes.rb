d2d1c1cbe83a0176aece4f6b35653e9d20c8908e
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-16 08:18:08 -0500

    Add authenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root "activities#index", as: :authenticated_root
+  end
+  root "static_pages#home"
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
   resources :activities
 
-  root "static_pages#home"
 end
