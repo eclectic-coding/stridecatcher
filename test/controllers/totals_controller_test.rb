2a2c79788c20d7c55f823828a641425f86c78c5b
Chuck <chuck@eclecticsaddlebag.com>
2021-02-17 12:02:58 -0500

    Total UI (#9)
    * Display weekly mileage
    * Write system test for total UI

---
+++ test/controllers/totals_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class TotalsControllerTest < ActionDispatch::IntegrationTest
+  # test "should get index" do
+  #   get totals_index_url
+  #   assert_response :success
+  # end
+end
