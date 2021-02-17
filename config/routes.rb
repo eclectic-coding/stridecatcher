015aa8c7d7473b541bbef187ba76d15ec923dd33
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-17 11:43:47 -0500

    Display weekly mileage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'totals/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   authenticated :user do
     root "activities#index", as: :authenticated_root
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
   }
 
   resources :activities
+  resources :totals, only: [:index]
 
 end
