be137c6da59f90bb6c9a7d6bc7dcbd38eb35bf22
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-22 07:56:00 -0500

    # If applied, this commit will...
    # Why is this change needed?
    Write pseudo code for notification logic
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -6,5 +6,18 @@ class Shoe < ApplicationRecord
   validates :allowed_distance_in_miles, numericality: { only_integer: true, greater_than_or_equal_to: 1,
                                                         allow_nil: true }
 
+  after_save :notify_user, if: :user_should_be_notified?
+
+  private
+
+  def notify_user
+    # send email
+    NotifyUserMailer.shoe_mileage_reached(self).deliver_now
+    self.notified = true
+  end
+  
+  def user_should_be_notified?
+    !self.notified? && self.distance_in_miles >= self.allowed_distance_in_miles
+  end
 
 end
