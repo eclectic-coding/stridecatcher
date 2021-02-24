aa7483d49897e756a85a56f3e8319fd7687e1d11
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-24 08:40:35 -0500

    Account for case where an associated shoe is destroyed

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -160,4 +160,13 @@ class ActivityTest < ActiveSupport::TestCase
 
     assert_equal 0.0, @shoe.reload.distance_in_miles
   end
+
+  test "should set shoe to nil if asssociated shoe is destroyed" do
+    @user = users(:confirmed_user_with_shoes)
+    @shoe = @user.shoes.first
+    @activity = @user.activities.create(distance: 10, unit: "miles", shoe: @shoe, date: Time.zone.now)
+    @activity.destroy
+
+    assert_nil @activity.reload.shoe
+  end
 end
